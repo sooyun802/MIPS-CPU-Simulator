1f0c9ad22b9ba1f51e44721393aec08cef1373d2
Gyeonghwan Hong <redcarrottt@gmail.com>
2016-11-22 07:33:51 +0900

    Program1 is completed

--- programs/program1.s
+++ programs/program1.s
@@ -2,11 +2,16 @@
 main:
   li    $3, 0       # $3 = i = 0
   li    $5, 0       # $5 = sum = 0
-  L1:
+L1:
+  li    $4, 0       # $4 = j = 0
+L2:
   add   $5, $5, $3  # sum += i
+  addi  $4, $4, 1   # j++
+  li    $6, 50
+  bne   $4, $6, L2  # if(j != 50) goto L2
   addi  $3, $3, 1   # i++
-  li    $6, 10
-  bne   $3, $6, L1  # if(i != 10) goto L1
+  li    $6, 1000
+  bne   $3, $6, L1  # if(i != 1000) goto L1
   b     exit
 exit:
   li $2, 10
